8bc3d6bbb2dcd3b94c6c74d6d1c0656445eae4ce
Ilya Denisov <i.denisovs@gmail.com>
2021-08-08 14:51:07 +0300

    Added basic routing.

--- sources/App.js
+++ sources/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Dashboard, Users } from './pages';
+
+import style from './App.scss';
 
 export default function App() {
     return (
         <Router>
             <NavBar />
+
+            <div className={style.PageLayout}>
+                <Switch>
+                    <Route exact path="/">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/users">
+                        <Users />
+                    </Route>
+                </Switch>
+            </div>
         </Router>
     )
 }
