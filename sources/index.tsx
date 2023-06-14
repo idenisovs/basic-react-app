fd58b8194207519814283e8caae17c4ecf884014
Ilja Denisovs <i.denisovs@gmail.com>
2023-06-14 10:21:27 +0300

    Migration to React 18.

--- sources/index.tsx
+++ sources/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App';
 import store from './store';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('app')!);
+
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('app')
+    </Provider>
 );
